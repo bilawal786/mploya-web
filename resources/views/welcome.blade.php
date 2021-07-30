8750777d25a5ecc72ca922d2f8e3eb9b80cdb2ac
sonu <bilawal.jabbar.786@gmail.com>
2021-07-30 12:19:27 +0500

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,60 +1,103 @@
 <!DOCTYPE html>
-<html>
-<style>
-    body, html {
-        height: 100%;
-        margin: 0;
-    }
+<html lang="en">
+<!--
 
-    .bgimg {
-        background-image: url('https://img.freepik.com/free-vector/dark-black-background-with-silver-lines_1017-31886.jpg?size=626&ext=jpg');
-        height: 100%;
-        background-position: center;
-        background-size: cover;
-        position: relative;
-        color: white;
-        font-family: "Courier New", Courier, monospace;
-        font-size: 25px;
-    }
+Page    : Static / Mobland
+Version : 1.0
+Author  : Surjith S M
+URI     : http://themeforest.net/user/surjithctly
 
-    .topleft {
-        position: absolute;
-        top: 0;
-        left: 16px;
-    }
+ -->
 
-    .bottomleft {
-        position: absolute;
-        bottom: 0;
-        left: 16px;
-    }
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Mobland - Responsive HTML5">
+    <meta name="keywords" content="HTML5, bootstrap, interior, fashion, shopping, responsive">
+    <title>Mploya</title>
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('front/vendors/fontawesome/css/font-awesome.min.css')}}">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="{{asset('front/vendors/simple-line-icons/css/simple-line-icons.css')}}">
+    <!-- Pe-icon Font -->
+    <link rel="stylesheet" href="{{asset('front/vendors/pe-icon/css/helper.css')}}">
+    <link rel="stylesheet" href="{{asset('front/vendors/pe-icon/css/pe-icon-7-stroke.css')}}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('front/vendors/bootstrap/css/bootstrap.min.css')}}">
+    <!-- owlcarousel Slider -->
+    <link rel="stylesheet" href="{{asset('front/vendors/owlcarousel/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('front/vendors/owlcarousel/css/owl.theme.default.min.css')}}">
+    <!-- Main css -->
+    <link href="{{asset('front/css/style.css')}}" rel="stylesheet"> </head>
 
-    .middle {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-    }
-
-    hr {
-        margin: auto;
-        width: 40%;
-    }
-</style>
 <body>
-
-<div class="bgimg">
-    <div class="topleft">
-        <p><img style="height: 100px" src="{{asset('image/mploya.jpeg')}}" alt=""></p>
+<div class="preloader" id="preloader"> <svg class="spinner" id="pageloader_anime" width="32px" height="32px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+    </svg> </div>
+<!--===================== Static No Scroll =====================-->
+<section class="space static">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="deatiled-phone-wrap"> <img src="{{asset('front/images/detailed-screen.png')}}" class="phone" alt="#">
+                    <div class="screen">
+                        <div class="screen-slider owl-carousel owl-theme">
+                            <div class="item"><img src="{{asset('front/images/01.jpg')}}" alt="#"></div>
+                            <div class="item"><img src="{{asset('front/images/02.jpg')}}" alt="#"></div>
+                            <div class="item"><img src="{{asset('front/images/03.jpg')}}" alt="#"></div>
+                            <div class="item"><img src="{{asset('front/images/04.jpg')}}" alt="#"></div>
+                            <div class="item"><img src="{{asset('front/images/05.jpg')}}" alt="#"></div>
+                            <div class="item"><img src="{{asset('front/images/06.jpg')}}" alt="#"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="static-content-wrap"> <img style="height: 50px" src="{{asset('front/images/logo2.png')}}" class="img-fluid" alt="#">
+                    <h1 style="font-weight: 900" class="mb-4 h2">Mploya Mobile Application for Job Seekers</h1>
+                    <h3>A dead simple mobile app for finding Job in your Area</h3>
+                    <p>Authoritatively underwhelm backward-compatible meta-services with premier leadership skills. Uniquely productize impactful e-services through cost effective applications. Energistically customize one-to-one ROI before leveraged process.</p>
+                </div>
+                <div class="static-list-block"> <span class="pe-7s-check"></span>
+                    <p>Employer & Job Seekers</p>
+                </div>
+                <div class="static-list-block"> <span class="pe-7s-check"></span>
+                    <p>Free Apply On Jobs</p>
+                </div>
+                <div class="static-list-block"> <span class="pe-7s-check"></span>
+                    <p>In App Job Interview</p>
+                </div>
+                <div class="static-img-block"> <a href="#"><img src="{{asset('front/images/app-store.png')}}" class="img-fluid" alt="#"></a> <a href="#">
+                        <img src="{{asset('front/images/play-store.png')}}" class="img-fluid" alt="#"></a>
+                    <p class="text-muted">Version required ios 10 or later. Android Kitkat or later</p>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="row mt-5">
+                <div class="col-md-6 ">
+                    <p>Copyright © 2020. All rights reserved. Mploya</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="static-links"> <a href="#">PRESS</a> <a href="#">TERMS</a> <a href="#">PRIVACY</a> </div>
+                </div>
+            </div>
+        </footer>
     </div>
-    <div class="middle">
-        <h1>COMING SOON</h1>
-        <hr>
-        <p>35 days left</p>
-    </div>
-
-</div>
-
+</section>
+<!--//End Static No Scroll -->
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script src="{{asset('front/vendors/jquery/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('front/vendors/bootstrap/js/popper.min.js')}}"></script>
+<script src="{{asset('front/vendors/bootstrap/js/bootstrap.min.js')}}"></script>
+<!-- owlcarousel JS -->
+<script src="{{asset('front/vendors/owlcarousel/js/owl.carousel.min.js')}}"></script>
+<!-- Script JS -->
+<script src="{{asset('front/js/script.js')}}"></script>
 </body>
+
 </html>

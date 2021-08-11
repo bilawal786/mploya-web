f72e4c465156a12f766f80bac8f80949c5b7976c
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-08-11 11:07:41 +0500

    new
    new

--- app/Http/Resources/AllJobCollection.php
+++ app/Http/Resources/AllJobCollection.php
@@ -18,23 +18,16 @@ class AllJobCollection extends JsonResource
      */
     public function toArray($request)
     {
-        $employer = User::find($this->employer_id);
-        $cat = Category::find($this->category_id);
-        $subcat = Subcategory::find($this->subcategory_id);
 
+        $employer = User::find($this->employer_id);
         return [
             'id' => $this->id,
             'employer_id' => $this->employer_id,
-            'category_name' => $cat->title,
-            // new
-            'subcategory_name' => $subcat->title,
             'requirements' => $this->requirements,
             'skills' => $this->skills,
             'link' => $this->link,
             'vacancies' => $this->vacancies,
             'job_type' => $this->job_type,
-
-            // end new
             'job_title' => $this->job_title,
             'company_name' => $employer->company_name,
             'employer_phone' => $employer->phone,

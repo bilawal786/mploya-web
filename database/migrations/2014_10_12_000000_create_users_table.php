511ca770272a8d044276cbadc2b1e65e3543bbeb
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-09-30 14:36:58 +0500

    new
    new

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -67,7 +67,7 @@ class CreateUsersTable extends Migration
             $table->string('profile_status')->nullable('visible');
             $table->string('provider_id')->nullable();
             $table->string('provider_name')->nullable();
-            $table->string('image')->default('assets/dist/img/profilepic.png');
+            $table->string('image')->default('assets/dist/img/avatar5.png');
             $table->string('video')->default('0');
             $table->string('facebook_link')->nullable();
             $table->string('instagram_link')->nullable();

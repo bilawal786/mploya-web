fc9297fa5c134b04515d81f123856aae141b4ef2
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-10-11 18:06:49 +0500

    new
    new

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -14,33 +14,33 @@ class PaymentController extends Controller
 {
     public function Payment($id, $userid)
     {
-        return view('payment.payment', ['id' => $id, 'userid' => $userid]);
+        return view('payment.payment', ['subscriptioniId' => $id, 'userid' => $userid]);
     }
 
     public function StripePayment(Request $request)
     {
-
+        $sub = Subscription::find($request->subscriptioniId);
         $purchasedsubscription = PruchasedSubscription::where('employer_id', '=', $request->userid)->first();
         if ($purchasedsubscription == null) {
             $subscription = new PruchasedSubscription();
             $subscription->employer_id = $request->userid;
-            $subscription->title = $request->title;
-            $subscription->price = $request->price;
-            $subscription->valid_job = $request->valid_job;
-            $subscription->status = $request->status;
-            $subscription->color = $request->color;
-            $subscription->description = $request->description;
+            $subscription->title = $sub->title;
+            $subscription->price = $sub->price;
+            $subscription->valid_job = $sub->valid_job;
+            $subscription->status = $sub->status;
+            $subscription->color = $sub->color;
+            $subscription->description = $sub->description;
             $subscription->save();
         } else {
             $purchasedsubscription = PruchasedSubscription::where('employer_id', '=', $request->userid)->first();
-            $purchasedsubscription->valid_job += $request->valid_job;
+            $purchasedsubscription->valid_job += $sub->valid_job;
             $purchasedsubscription->update();
         }
 
-        $subscription = Subscription::find($request->subscription_id);
+        // $subscription = Subscription::find($request->subscriptioniId);
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
         $cahrge = \Stripe\Charge::create([
-            "amount" => $subscription->price * 100,
+            "amount" => $sub->price * 100,
             "currency" => "usd",
             "source" => $request->stripeToken,
             "description" => "Payment for Subscription Purchase."

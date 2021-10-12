1c96d90d19ed5af653199cc969043a230e0e7b36
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-10-12 11:09:06 +0500

    new
    nrwe

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             // SubscriptionSeeder::class,
             // ActiveSunscriptionSeeder::class,
-            // SubcategorySeeder::class,
+            SubcategorySeeder::class,
             // BookmarkJobSeeder::class,
             // AppliedJobSeeder::class,
             // AppliedUserJobSeeder::class,

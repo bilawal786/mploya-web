7d742a88ecceca739f128f12666c5b44a39b24c1
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-11-29 12:23:34 +0500

    new
    new

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -11,17 +11,23 @@
 </style>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1></h1>
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <!-- <h1>DataTables</h1> -->
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Profile</li>
+                        </ol>
+                    </div>
                 </div>
-            </div>
-        </div><!-- /.container-fluid -->
-        <span id="result"></span>
-    </section>
+                <span id="result"></span>
+            </div><!-- /.container-fluid -->
+        </section>
+  
 
     <!-- Main content -->
     <section class="content">
